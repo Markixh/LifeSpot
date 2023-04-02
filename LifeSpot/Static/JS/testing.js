258e5608273aa4000fd54b3eeac79faeb2e5ea5d
Марков Андрей Анатольевич <andrejmarko@yandex.ru>
2023-04-02 20:21:41 +0300

    выполнены задания до 28.3

---
+++ LifeSpot/Static/JS/testing.js
@@ -0,0 +1,5 @@
+﻿let userName;
+userName = prompt("Пожалуйста, введите ваше имя");
+let count;
+count = userName.length;
+alert(`Приветствуем на LifeSpot ${userName}, в вашем имени ${count} символов`);
