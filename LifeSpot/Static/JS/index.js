6293bfa18902aa89144141d820378591f20ad802
Марков Андрей Анатольевич <andrejmarko@yandex.ru>
2023-04-03 20:30:12 +0300

    Итоги модуля № 28

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,4 +1,5 @@
 ﻿let age = prompt("Пожалуйста, введите ваш возраст");
+
 if (age >= 18) {
     let now = new Date();
     alert("Приветствуем на LifeSpot " + now);
@@ -6,4 +7,14 @@ if (age >= 18) {
 else {
     alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
     window.location.href = "http://www.google.com"
+}
+
+let session = new Map();
+
+session.set('age', age)
+session.set('userAgent', window.navigator.userAgent);
+session.set('startDate', new Date().toLocaleString());
+
+for (let result of session) {
+    console.log(result)
 }
--- LifeSpot/Static/JS/testing.js
+++ LifeSpot/Static/JS/testing.js
@@ -1,5 +1,2 @@
-﻿let userName;
-userName = prompt("Пожалуйста, введите ваше имя");
-let count;
-count = userName.length;
-alert(`Приветствуем на LifeSpot ${userName}, в вашем имени ${count} символов`);
+﻿let elements = document.getElementsByTagName('*');
+alert(`На странице LifeSpot ${elements.length} элементов`);
