fb8a9a2d69cbc4c239a36e89e4212fc87901b250
Марков Андрей Анатольевич <andrejmarko@yandex.ru>
2023-04-07 21:23:12 +0300

    Задание 29.1.1

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -18,3 +18,21 @@ session.set('startDate', new Date().toLocaleString());
 for (let result of session) {
     console.log(result)
 }
+
+function filter() {
+    let inputText = document.getElementsByTagName('input')[0].value.toLowerCase();
+    let elements = document.getElementsByClassName('video-container');
+
+    for (let i = 0; i <= elements.length; i++) {
+        let element = elements[i].getElementsByTagName('h3')[0].innerText;
+
+        console.log(element.toLowerCase().includes(inputText));
+
+        if (element.toLowerCase().includes(inputText)) {
+            elements[i].style.display = 'inline-block';
+        }
+        else {
+            elements[i].style.display = 'none';
+        }
+    }
+}
