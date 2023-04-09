aa281ed7cd38372b3d37ad7878b11fa6b524ac39
Марков Андрей Анатольевич <andrejmarko@yandex.ru>
2023-04-09 20:16:30 +0300

    Задание 29.6.7

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,4 +1,14 @@
-﻿function handleSession() {
+﻿function handleSession() {    
+    let session = new Map();
+
+    //session.set('age', checkAge())
+    session.set('userAgent', window.navigator.userAgent);
+    session.set('startDate', new Date().toLocaleString());
+
+    return session;
+}
+
+function checkAge() {
     let age = prompt("Пожалуйста, введите ваш возраст");
 
     if (age >= 18) {
@@ -9,17 +19,9 @@
         alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
         window.location.href = "http://www.google.com"
     }
-
-    let session = new Map();
-
-    session.set('age', age)
-    session.set('userAgent', window.navigator.userAgent);
-    session.set('startDate', new Date().toLocaleString());
-
-    return session;
+    return age
 }
-
-function printSession(session) {
+function printSession() {
     for (let result of session) {
         console.log(result)
     }
